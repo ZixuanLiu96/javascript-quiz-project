195ae9677daeb00bf3c2cbc407f5e30403273d57
Zixuan <liusherry08@gmail.com>
2025-06-28 17:58:10 +0100

    added filterQuestionsByDifficulty() and averageDifficulty() methods

--- src/question.js
+++ src/question.js
@@ -1,7 +1,23 @@
 class Question {
-    // YOUR CODE HERE:
-    //
-    // 1. constructor (text, choices, answer, difficulty)
+  // 1. constructor (text, choices, answer, difficulty)
+  constructor(text, choices, answer, difficulty) {
+    this.text = text;
+    this.choices = choices;
+    this.answer = answer;
+    this.difficulty = difficulty;
+  }
 
-    // 2. shuffleChoices()
+  // 2. shuffleChoices()
+  shuffleChoices() {
+    const newArr = [];
+    const random = Math.floor(Math.random() * this.choices.length);
+    for (let i = this.choices.length; i > 0; i--) {
+      newArr.push(this.choices[random]);
+      this.choices.splice(random, 1);
+    }
+    return newArr;
+  }
 }
+
+const question1 = new Question("11", [1, 2, 3, 4, 5], "123", 1);
+console.log(question1.shuffleChoices());
--- src/quiz.js
+++ src/quiz.js
@@ -1,15 +1,96 @@
 class Quiz {
-    // YOUR CODE HERE:
-    //
-    // 1. constructor (questions, timeLimit, timeRemaining)
+  // 1. constructor (questions, timeLimit, timeRemaining)
+  constructor(questions, timeLimit, timeRemaining) {
+    this.questions = questions;
+    this.timeLimit = timeLimit;
+    this.timeRemaining = timeRemaining;
+    this.correctAnswers = 0;
+    this.currentQuestionIndex = 0;
+  }
+  // 2. getQuestion()
+  getQuestion() {
+    return this.questions[this.currentQuestionIndex];
+  }
 
-    // 2. getQuestion()
-    
-    // 3. moveToNextQuestion()
+  // 3. moveToNextQuestion()
+  moveToNextQuestion() {
+    this.currentQuestionIndex++;
+  }
 
-    // 4. shuffleQuestions()
+  // 4. shuffleQuestions()
+  shuffleQuestions() {
+    const newQestionArr = [];
 
-    // 5. checkAnswer(answer)
+    for (let i = this.questions.length; i > 0; i--) {
+      const random = Math.floor(Math.random() * this.questions.length);
+      newQestionArr.push(this.questions[random]);
+    }
+    this.questions = newQestionArr;
+  }
 
-    // 6. hasEnded()
+  // 5. checkAnswer(answer)
+  checkAnswer(answer) {
+    if (answer === this.questions[this.currentQuestionIndex].answer) {
+      this.correctAnswers++;
+    }
+  }
+
+  // 6. hasEnded()
+  hasEnded() {
+    if (this.currentQuestionIndex < this.questions.length) return false;
+    return true;
+  }
+
+  // filterQuestionsByDifficulty()
+  filterQuestionsByDifficulty(difficulty) {
+    if (difficulty == 1 || difficulty == 2 || difficulty == 3) {
+      this.questions = this.questions.filter(
+        (question) => question.difficulty == difficulty
+      );
+    }
+  }
+
+  // averageDifficulty()
+  averageDifficulty() {
+    return (
+      this.questions.reduce((sum, question) => sum + question.difficulty, 0) /
+      this.questions.length
+    );
+  }
 }
+
+const questions = [
+  {
+    text: "Question 1",
+    choices: ["a", "b", "c"],
+    answer: "a",
+    difficulty: 1,
+  },
+  {
+    text: "Question 2",
+    choices: ["d", "e", "f"],
+    answer: "d",
+    difficulty: 2,
+  },
+  {
+    text: "Question 3",
+    choices: ["g", "h", "i"],
+    answer: "g",
+    difficulty: 2,
+  },
+  {
+    text: "Question 4",
+    choices: ["j", "k", "l"],
+    answer: "j",
+    difficulty: 1,
+  },
+  {
+    text: "Question 5",
+    choices: ["m", "n", "o"],
+    answer: "m",
+    difficulty: 3,
+  },
+];
+
+const quiz1 = new Quiz(questions, 60, 0, 0, 0);
+console.log(quiz1.averageDifficulty());
